9704d86146c28f47b027800f29272c588ce05fff
Conor Sheehan <conor@cantinaconsulting.com>
2015-05-25 16:13:34 -0400

    Add rectangular prism calculator

---
+++ chapters/01-tdd/spec/02-rectangular-prism-calculator-spec.js
@@ -0,0 +1,16 @@
+xdescribe('Rectangular Prism Calculator', function() {
+    it('calculates the surface area of a 10m x 10m x 6m prism', function() {
+    });
+
+    it('calculates the surface area of a 2m x 3m x 15m prism', function() {
+    });
+
+    it('calculates the volume of a 2m x 3m x 15m prism', function() {
+    });
+
+    it('calculates the diagonal of a 1m x 4m x 8m prism', function() {
+    });
+
+    it('calculates the diagonal of a 4m x 6m x 7m prism', function() {
+    });
+});
---
+++ chapters/01-tdd/src/02-rectangular-prism-calculator.js
@@ -0,0 +1,4 @@
+var rectangularPrismCalculator = (function() {
+    return {
+    };
+})();
