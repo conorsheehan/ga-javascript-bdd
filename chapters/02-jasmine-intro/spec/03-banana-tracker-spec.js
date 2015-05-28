93fb45c5b852f92adcb31eb792f4a8cc3aa0887f
Conor Sheehan <conor@cantinaconsulting.com>
2015-05-28 00:31:48 -0400

    Chapter 2: Add activity

---
+++ chapters/02-jasmine-intro/spec/03-banana-tracker-spec.js
@@ -0,0 +1,54 @@
+xdescribe('Banana Tracker', function() {
+
+    it('starts with no bananas in a bunch', function() {
+
+    });
+
+
+
+    it('can add bananas to a bunch', function() {
+
+    });
+
+
+
+    it('knows how many bananas are in the current bunch', function() {
+
+    });
+
+
+
+    it('can remove bananas from a bunch', function() {
+
+    });
+
+
+
+    it('keeps track of how many bananas were added to the bunch, whether or not they were later removed', function() {
+
+    });
+
+
+
+    it('marks the bunch as ready to ship once it has at least 5 bananas', function() {
+
+    });
+
+
+
+    it('keeps track of who is going to ship the bananas', function() {
+
+    });
+
+
+
+    it('can add a single sticker on the bananas using the sticker name', function() {
+
+    });
+
+
+
+    it('can add multiple stickers on the bananas using the sticker names', function() {
+
+    });
+});
---
+++ chapters/02-jasmine-intro/src/03-banana-tracker-app.js
@@ -0,0 +1,42 @@
+var simpleApp = (function() {
+
+    return {
+        initialize: function() {},
+
+        /** Adds a banana to the bunch */
+        addBanana: function() {},
+
+        /** Removes a banana to the bunch */
+        removeBanana: function() {},
+
+        /** Returns the number of bananas currently in the bunch */
+        getCurrentBananaCount: function() {},
+
+        /** Returns the number of bananas that have been added to the bunch */
+        getAllBananasAdded: function() {},
+
+        /** Returns the number of bananas that have been removed to the bunch */
+        getAllBananasRemoved: function() {},
+
+        /** Set the expiration date of the banana bunch */
+        setExpiration: function() {},
+
+        /** Returns the expiration date of the banana bunch */
+        getExpiration: function() {},
+
+        /** Returns whether or not the bunch is ready to be shipped */
+        bunchIsReady: function() {},
+
+        /** Set the name of the person responsible for shipping the bananas */
+        setShipper: function() {},
+
+        /** Set the name of the person responsible for shipping the bananas */
+        getShipper: function() {},
+
+        /** Adds a sticker to the bananas, by the sticker's name */
+        addSticker: function() {},
+
+        /** Returns the names of the various stickers on the bananas */
+        getStickers: function() {}
+    };
+})();
