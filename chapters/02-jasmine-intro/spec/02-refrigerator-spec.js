9e025f13efb3e5b2cd3b01837a964da286298b61
Conor Sheehan <conor@cantinaconsulting.com>
2015-10-26 16:15:46 -0400

    Add refrigerator spec

---
+++ chapters/02-jasmine-intro/spec/02-refrigerator-spec.js
@@ -0,0 +1,47 @@
+xdescribe('My refrigerator', function() {
+
+    var refrigerator = {
+        hasPower: true, // Has electrical power
+        temperature: 40, // Temperature in Fahrenheit
+        capacity: 14, // Inner capacity in cubic feet
+        outerDimensions: { // Outer dimensions in inches
+            height: 70,
+            width: 36,
+            depth: 31
+        },
+        contents: [{
+                name: 'Pizza',
+                qty: 1
+            }, {
+                name: 'Stir fry',
+                qty: 1
+            }, {
+                name: 'Beer',
+                qty: 4
+            }, {
+                name: 'Eggs',
+                qty: 12
+            }
+        ]
+    };
+
+    it('is running', function() {
+
+    });
+
+    it('is at 40 degrees Fahrenheit', function() {
+
+    });
+
+    it('can fit in a 75" tall room', function() {
+
+    });
+
+    it('can fit in a 40" x 40" nook', function() {
+
+    });
+
+    it('knows how much food it contains', function() {
+
+    });
+});
